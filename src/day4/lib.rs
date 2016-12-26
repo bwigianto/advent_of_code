8e3719b1e0449d3ba00c4271f0704d53892caf14
Bryan Wigianto <bwigianto@gmail.com>
2016-12-26 00:49:15 -0600

    done problem 4

--- src/day4/lib.rs
+++ src/day4/lib.rs
@@ -1,12 +1,58 @@
 use regex::Regex;
+use std::collections::HashMap;
+
+pub fn shift_str(s: &str, n: i32) -> String {
+  s.chars()
+    .fold(String::new(), |res, c| res + &format!("{}", shift(c, n)))
+}
+
+pub fn shift(c: char, n: i32) -> char {
+  if c == '-' { return '-'; }
+  let alpha = "abcdefghijklmnopqrstuvwxyz";
+  let index = (alpha.chars().position(|ch| ch == c).unwrap() + n as usize) % alpha.len();
+  alpha.chars().nth(index).unwrap()
+}
+
+pub fn real(code: &str) -> bool {
+    real_checksum(code_part(code)) == checksum(code)
+}
+
+pub fn real_checksum(code: &str) -> String {
+    let mut count: HashMap<char, u32> = HashMap::new();
+    for c in code.chars() {
+        if c == '-' { continue; }
+        *count.entry(c).or_insert(0) += 1;
+    }
+    let mut count_vec: Vec<(&char, &u32)> = count.iter().collect();
+    count_vec.sort_by(|a, b| {
+        if a.1 == b.1 {
+            return a.0.cmp(b.0);
+        }
+        b.1.cmp(a.1)
+    });
+    count_vec.iter()
+        .take(5)
+        .map(|e| e.0)
+        .fold(String::new(), |res, c| res + &format!("{}", c))
+}
+
+pub fn code_part(code: &str) -> &str {
+    let re = Regex::new("(.*)-\\d+\\[.*\\]$").unwrap();
+    capture(re, code)
+}
 
 pub fn checksum(code: &str) -> &str {
-    return ""
+    let re = Regex::new(".*\\[(.*)\\]").unwrap();
+    capture(re, code)
 }
 
 pub fn id(code: &str) -> i32 {
     let re = Regex::new(".*-(\\d+)\\[.*").unwrap();
-    return int(re.captures(code).unwrap().at(1).unwrap());
+    int(capture(re, code))
+}
+
+fn capture(re: Regex, s: &str) -> &str {
+    re.captures(s).unwrap().at(1).unwrap()
 }
 
 fn int(s: &str) -> i32 {
--- tests/test_day4.rs
+++ tests/test_day4.rs
@@ -3,6 +3,8 @@ extern crate advent_of_code;
 #[cfg(test)]
 mod tests {
   use advent_of_code::day4::lib;
+  use std::io::{BufReader,BufRead};
+  use std::fs::File;
 
   #[test]
   fn gets_id_correctly() {
@@ -10,12 +12,72 @@ mod tests {
   }
 
   #[test]
-  fn gets_id_correctly2() {
-      assert_eq!(lib::id("aaaaa-y-x-3214[blah]"), 3214);
+  fn gets_checksum() {
+    assert_eq!(lib::checksum("aaaaa-bbb-z-y-x-123[abxyz]"), "abxyz");
   }
 
   #[test]
-  fn gets_checksum() {
-      assert_eq!(lib::checksum("aaaaa-bbb-z-y-x-123[abxyz]"), "abxyz");
+  fn get_code() {
+    assert_eq!(lib::code_part("aaaaa-bbb-z-y-x-123[abxyz]"), "aaaaa-bbb-z-y-x");
+  }
+
+  #[test]
+  fn get_real_checksum() {
+    assert_eq!(lib::real_checksum("aaaa-bb-z-y-x"), "abxyz");
+  }
+
+  #[test]
+  fn get_real_checksum_stops_at_5_chars() {
+    assert_eq!(lib::real_checksum("aaaa-bb-z-y-x-c-d-e"), "abcde");
+  }
+
+  #[test]
+  fn real() {
+    assert_eq!(lib::real("aaaa-bbb-z-y-x-123[abxyz]"), true);
+  }
+  
+  #[test]
+  fn fake() {
+    assert_eq!(lib::real("aaaa-bbb-z-y-x-123[abxyzq]"), false);
+  }
+
+  #[test]
+  fn shift_char() {
+    assert_eq!(lib::shift('a', 1), 'b');
+    assert_eq!(lib::shift('b', 2), 'd');
+    assert_eq!(lib::shift('y', 2), 'a');
+  }
+
+  #[test]
+  fn shift_str() {
+    assert_eq!(lib::shift_str("xyz", 1), "yza");
+  }
+
+  #[test]
+  #[ignore]
+  fn part1() {
+     let file = File::open("src/day4/input.txt").unwrap();
+     let ct: i32 = BufReader::new(file).lines()
+         .map(|line| line.unwrap())
+         .filter(|line| lib::real(&line))
+         .map(|line| lib::id(&line))
+         .sum();
+     assert_eq!(ct, 409147);
+  }
+
+  #[test]
+  #[ignore]
+  fn part2() {
+    let file = File::open("src/day4/input.txt").unwrap();
+    for line in BufReader::new(file).lines()
+      .map(|line| line.unwrap())
+      .filter(|line| lib::real(&line)) {
+      let code = lib::code_part(&line);
+      let id = lib::id(&line);
+      let shifted = lib::shift_str(code, id);
+      if shifted.contains("north") {
+          println!("{}:{}", shifted, id);
+      }
+    }
   }
 }
